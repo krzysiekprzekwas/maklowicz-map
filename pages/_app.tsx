b7c245cd9dbe732301ccbf2a3b39418c48fcf361
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2025-03-29 12:00:12 +0100

    move title & limit mobile filters

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <div className="min-h-screen pb-[env(safe-area-inset-bottom)] flex flex-col">
+        <Head>
+          <title>Śladami Roberta Makłowicza</title>
+          <meta name="description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
+          
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://sladami-roberta.vercel.app" />
+          <meta property="og:title" content="Śladami Roberta Makłowicza" />
+          <meta property="og:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
+          <meta property="og:image" content="/og-image.png" />
+
+          {/*  Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://sladami-roberta.vercel.app" />
+          <meta property="twitter:title" content="Śladami Roberta Makłowicza" />
+          <meta property="twitter:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
+          <meta property="twitter:image" content="/og-image.png" />
+        </Head>
         <Header />
         <Component {...pageProps} className="flex-1" />
       </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,26 +4,10 @@ export default function Document() {
   return (
     <Html lang="pl">
         <Head>
-        <title>Śladami Roberta Makłowicza</title>
-        <meta name="description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
-        <meta name="google-site-verification" content="IBFJq-uNh-c6-ekw-kkNzUqEY7MY_lHT-J6mDKzyHrQ" />
-        
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://sladami-roberta.vercel.app" />
-        <meta property="og:title" content="Śladami Roberta Makłowicza" />
-        <meta property="og:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
-        <meta property="og:image" content="/og-image.png" />
+          <meta name="google-site-verification" content="IBFJq-uNh-c6-ekw-kkNzUqEY7MY_lHT-J6mDKzyHrQ" />
 
-        {/*  Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://sladami-roberta.vercel.app" />
-        <meta property="twitter:title" content="Śladami Roberta Makłowicza" />
-        <meta property="twitter:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
-        <meta property="twitter:image" content="/og-image.png" />
-
-        <meta name="keywords" content="Robert Makłowicz, mapa restauracji, atrakcje turystyczne, Makło, Dalmacja" />
-        <link rel="icon" href="/favicon.ico" />
+          <meta name="keywords" content="Robert Makłowicz, mapa restauracji, atrakcje turystyczne, Makło, Dalmacja" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
             <Main />
