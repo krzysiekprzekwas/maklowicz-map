da68def1333f1f864aad49904b9e24be9753aa77
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2025-04-28 00:11:33 +0200

    Fig the og paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,21 +10,20 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="min-h-screen pb-[env(safe-area-inset-bottom)] flex flex-col">
         <Head>
           <title>Śladami Roberta Makłowicza</title>
-          <meta name="description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
-          
-          {/* Open Graph / Facebook */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://sladami-roberta.vercel.app" />
-          <meta property="og:title" content="Śladami Roberta Makłowicza" />
-          <meta property="og:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
-          <meta property="og:image" content="/og-image.png" />
+          <meta name="description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza."/>
 
-          {/*  Twitter */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://sladami-roberta.vercel.app" />
-          <meta property="twitter:title" content="Śladami Roberta Makłowicza" />
-          <meta property="twitter:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza." />
-          <meta property="twitter:image" content="/og-image.png" />
+          <meta property="og:url" content="https://www.sladami-roberta.pl"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="Śladami Roberta Makłowicza"/>
+          <meta property="og:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza."/>
+          <meta property="og:image" content="https://sladami-roberta.pl/og-image.png"/>
+
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:domain" content="sladami-roberta.pl"/>
+          <meta property="twitter:url" content="https://www.sladami-roberta.pl"/>
+          <meta name="twitter:title" content="Śladami Roberta Makłowicza"/>
+          <meta name="twitter:description" content="Interaktywna mapa restauracji i atrakcji odwiedzonych przez Roberta Makłowicza."/>
+          <meta name="twitter:image" content="https://sladami-roberta.pl/og-image.png"/>
         </Head>
         <Header />
         <Component {...pageProps} className="flex-1" />
