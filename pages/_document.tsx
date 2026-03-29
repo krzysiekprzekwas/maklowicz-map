b55a9bc6fa7404486d62753f7fee8459092eba4e
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2026-03-29 17:58:47 +0200

    fix: favicon for apple

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,9 @@ export default function Document() {
           <meta name="google-site-verification" content="IBFJq-uNh-c6-ekw-kkNzUqEY7MY_lHT-J6mDKzyHrQ" />
 
           <meta name="keywords" content="Robert Makłowicz, mapa restauracji, atrakcje turystyczne, Makło, Dalmacja" />
-          <link rel="icon" href="/favicon.ico?v=2" />
-          <link rel="icon" type="image/ico" href="/favicon.ico?v=2" />
+          <link rel="icon" href="/favicon_hat.ico" />
+          <link rel="icon" type="image/x-icon" href="/favicon_hat.ico" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Reenie+Beanie&family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
