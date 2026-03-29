6c6ab736eff865348008fd91a0dd6ac9a02002c4
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2026-03-29 13:33:26 +0200

    feat: Favicon cache busting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
